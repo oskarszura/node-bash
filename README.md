93392f49c12b8d62aa314bca27155ea57af3b28f
oskarszura <oskar.szura@gmail.com>
2016-03-23 22:01:41 +0100

    Reimplement flags

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# node-bash cmd tool
+# node-bash 0.1v bash tool
+
+node-bash is a simple script which is just a shorthand to usefull nodejs related bash commands set. 
+
+### Currently supported flags
+* -l|--list - list all node processes
+* -k|--kill - kill all node processes
--- node-bash.sh
+++ node-bash.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-while getopts ":s:p:" opt; do
-  case $opt in
-    k)
-      #kill all node processes
-      ;;
-    l)
-      echo "$(ps | grep node)"
-      ;;
-    \?)
-      echo "Invalid argument '-$OPTARG'" >&2
-      exit 1
-      ;;
+function printHelp {
+  echo "------ node-bash HELP - 0.1v --------"
+  echo "Use script with one flag lister below"
+  echo "* -l|--list - list all node processes"
+  echo "* -k|--kill - kill all node processes"
+  echo "-------------------------------------"
+}
+
+while true ; do
+
+  case $1 in
+
+    -h|--help)
+        printHelp
+        ;;
+
+    -k|--kill)
+        killall node
+        ;;
+
+    -l|--list)
+        echo "$(ps aux | grep node)"
+        ;;
+
+    --)
+        echo "Empty flag"
+        ;;
+
     *)
-      echo "Default"
+      echo "Invalid flag"
       ;;
+
   esac
+
+  exit 1
+
 done
